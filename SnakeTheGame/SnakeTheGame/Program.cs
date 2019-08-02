ce07cbb8623a8d72fbf3183e7cdf7232a0e7528d
mustLead <bes965@mail.ru>
2019-08-02 13:42:06 +0300

    learn List's and foreach cycle

--- SnakeTheGame/SnakeTheGame/Program.cs
+++ SnakeTheGame/SnakeTheGame/Program.cs
@@ -16,6 +16,28 @@ namespace SnakeTheGame
             point p2 = new point(4, 5, '#');
             p2.Draw();
 
+            point p3 = new point(8, 4, '@');
+            p3.Draw();
+
+            point p4 = new point(5, 9, '&');
+            p4.Draw();
+
+            List<char> CharList = new List<char>();
+            CharList.Add('*');
+            CharList.Add('#');
+            CharList.Add('@');
+
+            foreach (char c in CharList)
+            {
+                Console.WriteLine(c);
+            }
+
+            List<point> pointList = new List<point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
+            pointList.Add(p3);
+            pointList.Add(p4);
+
             Console.ReadLine();
         }
     }
