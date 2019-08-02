65bf039c812cbcf7b06fba62539abc678f4061a0
mustLead <bes965@mail.ru>
2019-08-02 12:52:56 +0300

    procedural method. function

--- SnakeTheGame/SnakeTheGame/Program.cs
+++ SnakeTheGame/SnakeTheGame/Program.cs
@@ -10,8 +10,25 @@ namespace SnakeTheGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a Doctor");
+            int x1 = 1;
+            int y1 = 3;
+            char symb1 = '*';
+
+            Draw(x1, y1, symb1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symb2 = '#';
+
+            Draw(x2, y2, symb2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
