29b24b548addc1cd740fdbcdc759b3f160d72fb6
mustLead <bes965@mail.ru>
2019-08-02 13:04:46 +0300

    created class point

--- SnakeTheGame/SnakeTheGame/Program.cs
+++ SnakeTheGame/SnakeTheGame/Program.cs
@@ -10,25 +10,19 @@ namespace SnakeTheGame
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char symb1 = '*';
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '*';
+            p1.Draw();
 
-            Draw(x1, y1, symb1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char symb2 = '#';
-
-            Draw(x2, y2, symb2);
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symb = '#';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char symb)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symb);
         }
     }
 }
---
+++ SnakeTheGame/SnakeTheGame/point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeTheGame
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char symb;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+}
